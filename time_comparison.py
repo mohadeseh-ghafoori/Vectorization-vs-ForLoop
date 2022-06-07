82ceaef3229175d7f4570b6581c5125c69da9348
mohadeseh <m.ghafoori78@gmail.com>
2022-06-07 18:23:23 +0430

    compute dot of vectors by vectorization

---
+++ time_comparison.py
@@ -0,0 +1,9 @@
+import numpy as np
+import time
+#vectorization method for inner product of 2 vector 
+a=np.random.rand(10000000)
+b=np.random.rand(10000000)
+tic=time.time()
+c=np.dot(a,b)
+toc=time.time()
+print("vectorized version: "+ str(1000*(toc-tic)) +" ms")
