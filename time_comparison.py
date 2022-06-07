9883269a22bd603e6ae4e2e9d0ee9c31524860dc
mohadeseh <m.ghafoori78@gmail.com>
2022-06-07 18:28:50 +0430

    compute dot by for loop and each element of vector

--- time_comparison.py
+++ time_comparison.py
@@ -7,3 +7,10 @@ tic=time.time()
 c=np.dot(a,b)
 toc=time.time()
 print("vectorized version: "+ str(1000*(toc-tic)) +" ms")
+#for loop method 
+c=0
+tic=time.time()
+for i in range(10000000):
+    c+=a[i]*b[i]
+toc=time.time()
+print("for loop version: "+ str(1000*(toc-tic)) +" ms")
